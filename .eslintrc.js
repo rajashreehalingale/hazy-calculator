6c9b9d4d773906afd1aaff86f8746ed0566e53bf
Rajashree <rhalingale@gmail.com>
2020-07-03 08:56:17 -0400

    hazy-calculator update with clean code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
+/* eslint.disable */
 module.exports = {
   "env": {
     "es6": true,
     "node": true,
   },
-  "extends": "eslint:recommended",
+
   "parserOptions": {
     "ecmaVersion": 2018,
     "sourceType": "module"
