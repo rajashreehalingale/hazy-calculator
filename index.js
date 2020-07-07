5dc7f72ce839120c5bd3d35cf61f8b0cde184e7c
Rajashree <rhalingale@gmail.com>
2020-07-07 08:40:18 -0400

    updated Index using filter

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-
 function calculate(calculationSteps) {
   if (calculationSteps.length < 3) return 0
   let operation = ['/', '+', '-', '*']
   let num1, num2 = ''
 
-  calculationSteps.forEach(calculationStep => {
-    if (calculationStep === null) calculationStep = 0
+  // operation = calculationSteps.find((calculationStep) => (isNaN(calculationStep) && (operation.indexOf(calculationStep) >= 0)))
 
-    if ((!isNaN(calculationStep)) && (!num2 && !!num1)) num2 = calculationStep
+  calculationSteps = calculationSteps.filter(calculationStep => (!isNaN(calculationStep) && calculationStep !== '') || (isNaN(calculationStep) && (operation.indexOf(calculationStep) >= 0)))
 
-    if ((!isNaN(calculationStep)) && (!num1)) { num1 = calculationStep }
-
-    else if (isNaN(calculationStep) && operation.indexOf(calculationStep) >= 0) operation = calculationStep
-  })
+  num1 = (calculationSteps[0] === null) ? 0 : parseInt(calculationSteps[0])
+  operation = calculationSteps[1]
+  num2 = (calculationSteps[2] === null) ? 0 : parseInt(calculationSteps[2])
 
   return findAns(num1, num2, operation)
 }
@@ -28,3 +25,34 @@ function findAns(num1, num2, operation) {
 }
 
 module.exports = calculate
+
+function calculate(calculationSteps) {
+  if (calculationSteps.length < 3) return 0
+  let operation = ['/', '+', '-', '*']
+  let num1, num2 = ''function calculate(calculationSteps) {
+    if (calculationSteps.length < 3) return 0
+    let operation = ['/', '+', '-', '*']
+    let num1, num2 = ''
+
+    // operation = calculationSteps.find((calculationStep) => (isNaN(calculationStep) && (operation.indexOf(calculationStep) >= 0)))
+
+    calculationSteps = calculationSteps.filter(calculationStep => (!isNaN(calculationStep) && calculationStep !== '') || (isNaN(calculationStep) && (operation.indexOf(calculationStep) >= 0)))
+
+    num1 = (calculationSteps[0] === null) ? 0 : parseInt(calculationSteps[0])
+    operation = calculationSteps[1]
+    num2 = (calculationSteps[2] === null) ? 0 : parseInt(calculationSteps[2])
+
+    return findAns(num1, num2, operation)
+  }
+
+  function findAns(num1, num2, operation) {
+    switch (operation) {
+      case '+': return parseInt(num1) + parseInt(num2)
+      case '-': return parseInt(num1) - parseInt(num2)
+      case '*': return parseInt(num1) * parseInt(num2)
+      case '/': return (isFinite(parseInt(num1.value) / parseInt(num2.value))) ? parseInt(num1) / parseInt(num2) : 0
+      default: return NaN
+    }
+  }
+
+  module.exports = calculate
